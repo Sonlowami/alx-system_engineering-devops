9c292e6c2e3e1986a9d50e5945423d96aee8105d
Sonlowami <uwilowami64@outlook.com>
2022-11-18 21:54:48 +0200

    Fix bugs and solve new problems

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?[0-9]{11})\] \[to:(\+?[0-9]{11})\] \[flags:(.{12})\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?[0-9]{11})\]\[to:(\+?[0-9]{11})\]\[flags:(.{12})\]/).join(",")
