5f7e2db8e336d207d5607a68cafa9f53aa15f8ea
Sonlowami <uwilowami64@outlook.com>
2023-03-25 13:17:32 +0200

    change the resource type to file-line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,13 @@
 # Puppet code to set up a configuration file in a server
 
-File {'ssh_config':
-  path => '/home/sonlowami/alx/alx-system_engineering-devops/0x0B-ssh/config',
-  ensure => file,
-  content => "Host *\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no\n",
+file-line {'Declaring identity file':
+  path => '/etc/ssh/ssh_config',
+  ensure => present,
+  line => '\tIdentityFile ~/.ssh/school',
+}
+
+file-line {'Deactivating Password authentication':
+  path =>'/etc/ssh/ssh_config',
+  ensure => present,
+  line => '\tPasswordAuthentication no',
 }
