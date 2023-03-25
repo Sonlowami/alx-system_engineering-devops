b1a3d7005f5ac1dddea1d7a0d1ad57c9761b28f5
Sonlowami <uwilowami64@outlook.com>
2023-03-25 13:23:43 +0200

    Fix a typo

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Puppet code to set up a configuration file in a server
 
-file-line {'Declaring identity file':
+file_line {'Declaring identity file':
   path => '/etc/ssh/ssh_config',
   ensure => present,
   line => '    IdentityFile ~/.ssh/school',
 }
 
-file-line {'Deactivating Password authentication':
+file_line {'Deactivating Password authentication':
   path =>'/etc/ssh/ssh_config',
   ensure => present,
   line => '    PasswordAuthentication no',
