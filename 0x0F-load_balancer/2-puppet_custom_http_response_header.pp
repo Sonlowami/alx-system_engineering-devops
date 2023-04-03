2ff20712ab93c417a27565aa8b37de57d173099b
Sonlowami <uwilowami64@outlook.com>
2023-04-03 22:10:25 +0200

    Configure as for new ubuntu machine

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,29 @@
-# Puppet header to create a custom header
+# Puppet header to create a custom header and configure a brand new ubuntu machine with nginx
+
+package {'nginx':
+  ensure => latest,
+}
+
+exec {'ufw-allow':
+  command => ufw allow 80/http,
+}
+
+exec {'hello':
+  command => echo 'Hello World!' |  tee /var/www/html/index.nginx-debian.html,
+}
+
+exec {'redirect':
+  command => sed -i '/^\tserver_name _.*/a \\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
+}
+
+exec {'Error 404':
+  command => sed -i '/^http.*/a \\terror_page 404 /error404.html;' /etc/nginx/nginx.conf; echo "Ceci n'est pas une page" | tee /var/www/html/error404.html,
+}
 exec {'X-Served-By':
   command  => sed -i "/^http.*/a \\\tadd_header X-Served-By $(hostname);" /etc/nginx/nginx.conf,
   provider => shell,
+}
+
+exec {'Restart':
+  command => service nginx restart,
 }
