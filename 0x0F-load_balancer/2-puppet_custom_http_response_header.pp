4cf0cd031c268429a0060810aad13e41224b01d4
Sonlowami <uwilowami64@outlook.com>
2023-04-09 19:24:26 +0200

    use command directories

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,23 +4,19 @@ package {'nginx':
   ensure => latest,
 }
 
-exec {'ufw-allow':
-  command => ufw allow 80/http,
-}
-
 exec {'hello':
-  command => echo 'Hello World!' |  tee /var/www/html/index.nginx-debian.html,
+  command => "/bin/echo 'Hello World!' |  /bin/tee /var/www/html/index.nginx-debian.html",
 }
 
 exec {'redirect':
-  command => sed -i '/^\tserver_name _.*/a \\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
+  command => "/bin/sed -i '/^\tserver_name _.*/a \\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default",
 }
 
 exec {'Error 404':
-  command => sed -i '/^http.*/a \\terror_page 404 /error404.html;' /etc/nginx/nginx.conf; echo "Ceci n'est pas une page" | tee /var/www/html/error404.html,
+  command => "/bin/sed -i '/^http.*/a \\terror_page 404 /error404.html;' /etc/nginx/nginx.conf; /bin/echo 'Ceci n\'est pas une page' | /bin/tee /var/www/html/error404.html",
 }
 exec {'X-Served-By':
-  command  => sed -i "/^http.*/a \\\tadd_header X-Served-By $(hostname);" /etc/nginx/nginx.conf,
+  command  => " /bin/sed -i '/^http.*/a \\tadd_header X-Served-By $(hostname);' /etc/nginx/nginx.conf",
   provider => shell,
 }
 
