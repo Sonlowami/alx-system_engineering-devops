0b0bc0101b106d73337e1f9fb453830208725408
Sonlowami <uwilowami64@outlook.com>
2023-05-09 17:40:06 +0200

    Catch a JSONDecodeError

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,6 +2,7 @@
 """Contain a function that prints all hot titles in a subreddit"""
 
 import requests
+import json
 
 
 def recurse(subreddit, hot_list=[], counter=0, after=''):
@@ -18,6 +19,6 @@ def recurse(subreddit, hot_list=[], counter=0, after=''):
         if after is None:
             raise AttributeError
         [hot_list.append(post['data']['title']) for post in posts]
-    except (KeyError, AttributeError):
+    except (KeyError, AttributeError, json.JSONDecodeError):
         return None if len(hot_list) == 0 else hot_list
     return recurse(subreddit, hot_list, counter, after)
